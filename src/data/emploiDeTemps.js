ca9552008c39e8db40bdab03badeaae1213f0850
Lino004 <h.ayinlamouhayad@gmail.com>
2019-09-01 00:13:02 +0100

    ajout emploi de temps

--- src/data/emploiDeTemps.js
+++ src/data/emploiDeTemps.js
@@ -213,6 +213,436 @@ const data = [
         }
       }
     ]
+  },
+  {
+    classe: 'Tle',
+    serie: 'A2',
+    section: '',
+    planning: [
+      {
+        autre: '',
+        lun: {
+          cours: 'Mathématique',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Codjo B.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        mar: {
+          cours: 'Histoire-Géographie',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Anderson S.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        mer: {
+          cours: 'Philosophie',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Raphael B.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        jeu: {
+          cours: 'Histoire-Géographie',
+          debut: '08h00',
+          fin: '10h00',
+          prof: 'Anderson S.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        ven: {
+          cours: 'Français',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Wesley D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'Anglais',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Moustapha S.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mar: {
+          cours: 'SVT',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Gires A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mer: {
+          cours: 'Anglais',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Moustapha S.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        jeu: {
+          cours: 'Philosophie',
+          debut: '10h00',
+          fin: '12h00',
+          prof: '',
+          url: ''
+        },
+        ven: {
+          cours: 'SVT',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Max S.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'Français',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Wesley D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mar: {
+          cours: 'Français',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Wesley D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mer: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        jeu: {
+          cours: 'Sport',
+          debut: '16h00',
+          fin: '19h00',
+          prof: 'Bilal C.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        ven: {
+          cours: 'Mathématique',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Codjo B.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'SVT',
+          debut: '17h00',
+          fin: '19h00',
+          prof: 'Gires A',
+          url: ''
+        },
+        mar: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        mer: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        jeu: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        ven: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      }
+    ]
+  },
+  {
+    classe: '3e',
+    serie: 'ML',
+    section: 'A',
+    planning: [
+      {
+        autre: '',
+        lun: {
+          cours: 'Espagnol',
+          debut: '08h00',
+          fin: '10h00',
+          prof: 'Alex D.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        mar: {
+          cours: 'Anglais',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Nicolas H.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        mer: {
+          cours: 'Mathématique',
+          debut: '07h00',
+          fin: '10h00',
+          prof: 'Clément D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        jeu: {
+          cours: 'Anglais',
+          debut: '08h00',
+          fin: '10h00',
+          prof: 'Nicolas H.',
+          url: '/tab-proFil/image@2x6.png'
+        },
+        ven: {
+          cours: 'Espagnol',
+          debut: '08h00',
+          fin: '10h00',
+          prof: 'Alex D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'SVT',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Soumaila C.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mar: {
+          cours: 'Mathématique',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Dossou C.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mer: {
+          cours: ' SPCT',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Afiz A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        jeu: {
+          cours: 'SVT',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Soumaila C',
+          url: '/tab-profil/image@2x6.png'
+        },
+        ven: {
+          cours: 'EPS',
+          debut: '10h00',
+          fin: '12h00',
+          prof: 'Charlie A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'Histoire-Géographie',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Justin S.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mar: {
+          cours: 'Français',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Charlie A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mer: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        jeu: {
+          cours: 'Histoire-Géographie',
+          debut: '15h00',
+          fin: '17h00',
+          prof: 'Justin S.',
+          url: '/tab-profil/image@2x1.png'
+        },
+        ven: {
+          cours: 'Mathématique',
+          debut: '15h00',
+          fin: '18h00',
+          prof: 'Clément D.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      },
+      {
+        autre: '',
+        lun: {
+          cours: 'SPCT',
+          debut: '17h00',
+          fin: '19h00',
+          prof: 'Afiz A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mar: {
+          cours: 'TD',
+          debut: '17h00',
+          fin: '19h00',
+          prof: 'Anges A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        mer: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        jeu: {
+          cours: 'Français',
+          debut: '17h00',
+          fin: '19h00',
+          prof: 'Charlie A.',
+          url: '/tab-profil/image@2x6.png'
+        },
+        ven: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        sam: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        },
+        dim: {
+          cours: '',
+          debut: '',
+          fin: '',
+          prof: '',
+          url: ''
+        }
+      }
+    ]
   }
 ]
 
--- src/views/app/emploiDeTemps/index.vue
+++ src/views/app/emploiDeTemps/index.vue
@@ -40,11 +40,7 @@
         <b-colxx md="8" lg="6" class="text-right text-center">
           <b-row class="h-100" align-v="center" align-h="end">
             <b-colxx md="3">
-              <b-button
-                class="px-1"
-                block variant="primary">
-                Ajouter <i class="simple-icon-plus"/>
-              </b-button>
+              <modal-vue/>
             </b-colxx>
             <b-colxx md="3"><b-button class="px-1" block variant="primary">Exporter <i class="iconsminds-right-1"/></b-button></b-colxx>
           </b-row>
@@ -102,9 +98,10 @@
 
 <script>
 import listeEmploi from '@/data/emploiDeTemps.js'
+import ModalVue from './ModalAdd'
 
 export default {
-  components: {},
+  components: { ModalVue },
   data () {
     return {
       selectClasse: null,
@@ -119,100 +116,23 @@ export default {
         'Vendredi',
         'Samedi',
         'Dimanche'
-      ],
-      planning: [
-        {
-          heure: '08h00 à 10h00',
-          lun: 'Français',
-          mar: 'Histoire-géo',
-          mer: 'Anglais',
-          jeu: '',
-          ven: '',
-          sam: '',
-          dim: ''
-        },
-        {
-          heure: '10h00 à 10h15',
-          lun: 'Récréation',
-          mar: 'Récréation',
-          mer: 'Récréation',
-          jeu: 'Récréation',
-          ven: 'Récréation',
-          sam: 'Récréation',
-          dim: ''
-        },
-        {
-          heure: '10h00 à 12h00',
-          lun: 'Français',
-          mar: 'Français',
-          mer: 'Français',
-          jeu: 'Français',
-          ven: 'Français',
-          sam: 'Français',
-          dim: ''
-        },
-        {
-          heure: '12h00 à 15h00',
-          lun: 'Pause',
-          mar: 'Pause',
-          mer: 'Pause',
-          jeu: 'Pause',
-          ven: 'Pause',
-          sam: 'Pause',
-          dim: ''
-        },
-        {
-          heure: '15h00 à 17h00',
-          lun: 'Français',
-          mar: 'Français',
-          mer: 'Français',
-          jeu: 'Français',
-          ven: 'Français',
-          sam: 'Français',
-          dim: ''
-        }
       ]
     }
   },
   computed: {
     classes () {
       const data = [{ value: null, text: 'Classe' }]
-      if (this.selectSection) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.classe)
-          if (!find && el.classe) data.push({ value: el.classe, text: el.classe })
-        })
-      }
-      if (this.selectSerie) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.classe)
-          if (!find && el.classe) data.push({ value: el.classe, text: el.classe })
-        })
-      }
-      if (!this.selectSection && !this.selectSerie) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.classe)
-          if (!find && el.classe) data.push({ value: el.classe, text: el.classe })
-        })
-      }
+      listeEmploi.forEach(el => {
+        const find = data.find(x => x.value === el.classe)
+        if (!find && el.classe) data.push({ value: el.classe, text: el.classe })
+      })
       return data
     },
     sections () {
       const data = [{ value: null, text: 'Section' }]
       if (this.selectClasse) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.section)
-          if (!find && el.section) data.push({ value: el.section, text: el.section })
-        })
-      }
-      if (this.selectSerie) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.section)
-          if (!find && el.section) data.push({ value: el.section, text: el.section })
-        })
-      }
-      if (!this.selectClasse && !this.selectSerie) {
-        this.list.forEach(el => {
+        const dataList = listeEmploi.filter(el => el.classe === this.selectClasse)
+        dataList.forEach(el => {
           const find = data.find(x => x.value === el.section)
           if (!find && el.section) data.push({ value: el.section, text: el.section })
         })
@@ -222,19 +142,8 @@ export default {
     series () {
       const data = [{ value: null, text: 'Serie' }]
       if (this.selectClasse) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.serie)
-          if (!find && el.serie) data.push({ value: el.serie, text: el.serie })
-        })
-      }
-      if (this.selectSection) {
-        this.list.forEach(el => {
-          const find = data.find(x => x.value === el.serie)
-          if (!find && el.serie) data.push({ value: el.serie, text: el.serie })
-        })
-      }
-      if (!this.selectClasse && !this.selectSection) {
-        this.list.forEach(el => {
+        const dataList = listeEmploi.filter(el => el.classe === this.selectClasse)
+        dataList.forEach(el => {
           const find = data.find(x => x.value === el.serie)
           if (!find && el.serie) data.push({ value: el.serie, text: el.serie })
         })
